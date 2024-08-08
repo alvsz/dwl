6a37012ad5aa26455c015201255502ca4cf8db68
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-08-08 14:42:16 -0600

    account border width in configurex11()
    Fixes: 13925eb1da8af2c1d23ee9d01efd03c3626081b2

--- src/xwayland.c
+++ src/xwayland.c
@@ -34,8 +34,8 @@ void configurex11(struct wl_listener *listener, void *data) {
   if ((c->isfloating && c != grabc) || client_is_unmanaged(c) ||
       !c->mon->lt[c->mon->sellt]->arrange)
     resize(c,
-           (struct wlr_box){.x = event->x,
-                            .y = event->y,
+           (struct wlr_box){.x = event->x - c->bw,
+                            .y = event->y - c->bw,
                             .width = event->width + c->bw * 2,
                             .height = event->height + c->bw * 2},
            0);
