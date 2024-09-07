19f2b4e0520abe25c1cffab9952770c19ada1555
Guido Cella <guido@guidocella.xyz>
2024-09-07 21:22:40 +0200

    fix a use after free
    This line makes dwl crash after closing mpv with the switchtotag patch.

--- src/dwl.c
+++ src/dwl.c
@@ -946,7 +946,6 @@ void defaultgaps(const Arg *arg) { setgaps(gappoh, gappov, gappih, gappiv); }
 
 void destroydecoration(struct wl_listener *listener, void *data) {
   Client *c = wl_container_of(listener, c, destroy_decoration);
-  c->decoration = NULL;
 
   wl_list_remove(&c->destroy_decoration.link);
   wl_list_remove(&c->set_decoration_mode.link);
