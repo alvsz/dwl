f5f6050f1277a12794df63a57c3f042924b78aeb
Leonardo Hernández Hernández <leohdz172@proton.me>
2024-10-27 20:08:02 -0600

    specify version for presentation-time (wlroots!4858)
    References: https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/4858

--- src/dwl.c
+++ src/dwl.c
@@ -2291,7 +2291,7 @@ void setup(void) {
   wlr_viewporter_create(dpy);
   wlr_single_pixel_buffer_manager_v1_create(dpy);
   wlr_fractional_scale_manager_v1_create(dpy, 1);
-  wlr_presentation_create(dpy, backend);
+  wlr_presentation_create(dpy, backend, 2);
   wlr_alpha_modifier_v1_create(dpy);
 
   /* Initializes the interface used to implement urgency hints */
