8fe4fe69f877c11a59b077e4badfc1f6f9415183
Joao Gabriel Alves <joao.aac@disroot.org>
2024-12-15 18:26:27 -0300

    faltou mudar aqui

--- config.h
+++ config.h
@@ -110,7 +110,7 @@ static enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_
 
 /* commands */
 static const char *termcmd[] = {"alacritty", NULL};
-static const char *menucmd[] = {"ags", "-t", "applauncher", NULL};
+static const char *menucmd[] = {"agsv1", "-t", "applauncher", NULL};
 
 /* named scratchpads - First arg only serves to match against key in rules*/
 static const char *scratchcmd[] = { "t", APP_TERM, "--class", CLASS_SP, "-t", TITLE_SP, NULL };
