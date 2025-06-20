e62641f22cf7673b8ba08b5cc9e92bd00ae727bd
Joao Gabriel Alves <joao.aac@disroot.org>
2025-06-20 14:26:42 -0300

    fix typo

--- include/config.h
+++ include/config.h
@@ -111,7 +111,7 @@ static enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TAP_MAP_
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-#define LUACMD(cmd) { .v = (const char*[]{ cmd, NULL }) }
+#define LUACMD(cmd) { .v = (const char*[]){ cmd, NULL }) }
 
 /* commands */
 static const char *termcmd[] = {"alacritty", NULL};
